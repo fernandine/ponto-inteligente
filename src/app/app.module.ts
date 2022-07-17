49462fc7afad8bcd613a8fc860a7105a873e372f
Jean Fernandine <jfernandine@gmail.com>
2022-07-17 17:37:19 -0300

    modelo de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,53 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatToolbarModule,
+  MatIconModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {LoginModule} from './autentication';
-
+import { 
+  LoginModule, 
+  LoginRoutingModule, 
+  CadastroPjModule,
+  CadastroPjRoutingModule,
+  CadastroPfModule,
+  CadastroPfRoutingModule
+} from './autenticacao';
+import { AppRoutingModule } from './app-routing.module';
+import { 
+  FuncionarioModule, 
+  FuncionarioRoutingModule 
+} from './funcionario';
+import {
+  AdminModule,
+  AdminRoutingModule
+} from './admin';
 
 @NgModule({
   declarations: [
-    AppComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    LoginModule
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule,
+    LoginModule,
+    LoginRoutingModule,
+    CadastroPjModule,
+    CadastroPjRoutingModule,
+    CadastroPfModule,
+    CadastroPfRoutingModule,
+    FuncionarioModule,
+    FuncionarioRoutingModule,
+    AdminModule,
+    AdminRoutingModule,
+
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
